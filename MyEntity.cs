78c3daeb8d0110bac13eb6cd06a4274cfe38c368
juan serrano <germgerm@gmail.com>
2016-12-07 01:01:38 -0800

    Separate WinterEntity into own file

--- MyEntity.cs
+++ MyEntity.cs
@@ -1,30 +1,10 @@
 using System.Reflection;
 
-namespace germ13{
+namespace germ13
+{
 
-public interface MyEntity{
-    void parse(string row);
-}
-
-public abstract class WintegrateEntity : MyEntity {
-  public abstract void parse(string row);
-
-  public void getFields(){
-
-  }
-
-  public PropertyInfo[] getProperties()
-  {
-      var props = (base.GetType()).GetProperties();
-      int count = props.Length;
-      PropertyInfo[] pi = new PropertyInfo[count];
-      count = 0;
-      foreach (var p in props)
-      {
-          pi[count] = p;
-          count++;
-      }
-      return pi;
-  }
-}
+    public interface MyEntity
+    {
+        void parse(string row);
+    }
 }
---
+++ WintegrateEntity.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+
+namespace germ13
+{
+    public abstract class WintegrateEntity : MyEntity
+    {
+        public abstract void parse(string row);
+
+        public void getFields()
+        {
+
+        }
+
+        public PropertyInfo[] getProperties()
+        {
+            var props = (base.GetType()).GetProperties();
+            int count = props.Length;
+            PropertyInfo[] pi = new PropertyInfo[count];
+            count = 0;
+            foreach (var p in props)
+            {
+                pi[count] = p;
+                count++;
+            }
+            return pi;
+        }
+
+    }
+}
